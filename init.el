fe389c06d9de3068dae63522b4ba82da27fc60de
GNU <gnu@c.(none)>
2012-11-13 16:34:27 +0530

    transient mode does not highlight after resume

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
 (fset'yes-or-no-p 'y-or-n-p) ; "y or n" instead of "yes or no"
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups"))) ; backup files directory
 (setq inhibit-startup-screen t) ; hide the splashscreen in order for the resume to work
-(add-hook 'emacs-startup-hook '(lambda() (resume 1))) ; resume while opening
+(add-hook 'emacs-startup-hook '(lambda() (resume 1) (keyboard-quit))) ; resume while opening, (keyboard-quit) fn makes the visible mark to be stopped.
 (add-hook 'before-save-hook 'delete-trailing-whitespace) ;delete trailing white-spaces before saving
 (transient-mark-mode t) ; highlight the region when the mark is active
 (require 'color-theme) ; color theme
@@ -33,5 +33,5 @@
 (require 'buffer-move)
 ; experimental bindings
 (global-set-key [(control ?\;)] 'backward-kill-word)
-(load (concat (getenv "HOME") "/.emacs_packages/xscheme/xscheme.el"))
+;(load (concat (getenv "HOME") "/.emacs_packages/xscheme/xscheme.el"))
 (load (concat (getenv "HOME") "/.emacs.d/org_settings.el"))
